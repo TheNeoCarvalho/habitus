e80666115d860ebe1ed5fb564e294ef8bad0f069
Manoel Carvalho <francisco.carvalho@certsys.com.br>
2023-03-06 15:50:34 -0300

    Initial Commit

---
+++ Habitus/HabitusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitusApp.swift
+//  Habitus
+//
+//  Created by Francisco Manoel Carvalho on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
